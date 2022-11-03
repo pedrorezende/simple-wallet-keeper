a67922f11131011256a4bd65b41b1e27266fca05
Pedro Rezende <xpedrorezende@gmail.com>
2022-11-03 17:07:00 -0300

    feat: setting up next-translate

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,6 @@
+import useTranslation from "next-translate/useTranslation";
+
 export default function Home() {
-  return <h1 className="text-xl font-bold underline">Hello world!</h1>;
+  const { t } = useTranslation();
+  return <h1 className="text-xl font-bold underline">{t("common:hello")}</h1>;
 }
