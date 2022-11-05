25b5cb5274394ec162b3be5fb624c26334a73a2a
Pedro Rezende <xpedrorezende@gmail.com>
2022-11-05 12:42:12 -0300

    feat: applying ibm plex sans font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      body: ["IBM Plex Sans", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
