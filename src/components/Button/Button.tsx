23e7c9bf7d3e61e0b0c4e3f4d76d5950d684b80b
Pedro Rezende <xpedrorezende@gmail.com>
2022-11-09 01:20:46 -0300

    feat: writing tests for Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,13 +2,14 @@ import React from "react";
 
 export interface ButtonProps {
   text: string | React.ReactNode;
-  type: "submit" | "button";
+  type?: "submit" | "button";
   onClick?: () => void;
 }
 
 export function Button({ text, type = "submit", onClick }: ButtonProps) {
   return (
     <button
+      role="button"
       onClick={onClick ? onClick : () => {}}
       className="uppercase relative px-9 py-2 text-sm text-gray-900 bg-yellow-400 rounded-sm shadow-sm active:shadow-none active:top-0.5"
       type={type}
--- src/components/Button/tests/Button.test.tsx
+++ src/components/Button/tests/Button.test.tsx
@@ -1,9 +1,20 @@
 import React from "react";
-
+import { fireEvent, render, screen } from "@testing-library/react";
 import { Button } from "../Button";
 
 describe("<Button />", () => {
-  it("tests are setting up correctly", () => {
-    expect(true).toBe(true);
+  it("should render text correctly", () => {
+    const text = "Lorem ipsum";
+    render(<Button text={text} />);
+    expect(screen.getByRole("button")).toHaveTextContent(text);
+    expect(screen).toMatchSnapshot();
+  });
+
+  it("should fire onclick callback correctly", () => {
+    const onClick = jest.fn();
+    render(<Button onClick={onClick} text="_" />);
+    fireEvent.click(screen.getByRole("button"));
+    expect(onClick).toHaveBeenCalled();
+    expect(screen).toMatchSnapshot();
   });
 });
