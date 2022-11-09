bd10f28e29e70f9a8091b3f03e87518e8e6041f3
Pedro Rezende <xpedrorezende@gmail.com>
2022-11-09 04:14:11 -0300

    refactor: removing unused component

--- src/components/WalletDetails/WalletDetails.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export interface WalletDetailsProps {
-  prop?: string;
-}
-
-export function WalletDetails({ prop = "default value" }: WalletDetailsProps) {
-  return <div>WalletDetails {prop}</div>;
-}
--- src/components/WalletDetails/tests/WalletDetails.test.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-import { WalletDetails } from "../WalletDetails";
-
-describe("<WalletDetails />", () => {});
