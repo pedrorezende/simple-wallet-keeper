b3ec7e4a1fd1fe4fa1b87196d4661cf6a5278983
Pedro Rezende <xpedrorezende@gmail.com>
2022-11-09 00:04:24 -0300

    feat: fixing scroll when container grows

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -7,7 +7,7 @@ type ContainerProps = {
 export function Container({ children }: ContainerProps) {
   return (
     <section className="flex items-center justify-center h-screen">
-      <div className="container w-full px-7 py-6 bg-white rounded-md shadow-sm min-h-[25vh] max-h-[70vh]">
+      <div className="container w-full my-4 px-7 py-6 bg-white rounded-md shadow-sm min-h-[25vh] max-h-[70vh] overflow-auto">
         {children}
       </div>
     </section>
